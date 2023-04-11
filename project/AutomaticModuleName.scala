55144187f2d8e4cd82a3189fa405f9116d72f1f5
Matthew de Detrich <matthew.dedetrich@aiven.io>
2023-04-11 20:37:52 +0200

    Add Apache license headers to source files

--- project/AutomaticModuleName.scala
+++ project/AutomaticModuleName.scala
@@ -1,6 +1,16 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
 /**
  * Copyright (C) 2009-2018 Lightbend Inc. <http://www.lightbend.com>
  */
+
 import sbt.{ Def, _ }
 import sbt.Keys._
 
