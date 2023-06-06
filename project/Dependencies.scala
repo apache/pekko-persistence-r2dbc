0fa9899eb9bdbb2ebf14ec20770bd102110fd0e7
Matthew de Detrich <matthew.dedetrich@aiven.io>
2023-06-06 19:48:46 +0200

    Update Scala 2 versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,8 +14,8 @@
 import sbt._
 
 object Dependencies {
-  val Scala212 = "2.12.17"
-  val Scala213 = "2.13.10"
+  val Scala212 = "2.12.18"
+  val Scala213 = "2.13.11"
   val PekkoVersion = System.getProperty("override.pekko.version", "0.0.0+26656-898c6970-SNAPSHOT")
   val PekkoVersionInDocs = "current"
   val PekkoProjectionVersion = "0.0.0+32-a55714ec-SNAPSHOT"
