8304a1253f10534aeb2d5ac035c4c64d373102db
PJ Fanning <pjfanning@users.noreply.github.com>
2023-10-31 18:33:24 +0000

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,8 +17,8 @@ object Dependencies {
   val Scala212 = "2.12.18"
   val Scala213 = "2.13.11"
   val Scala3 = "3.3.0"
-  val PekkoVersion = System.getProperty("override.pekko.version", "1.0.0")
-  val PekkoVersionInDocs = "1.0.0"
+  val PekkoVersion = System.getProperty("override.pekko.version", "1.0.1")
+  val PekkoVersionInDocs = "1.0.1"
   val PekkoPersistenceJdbcVersion = "1.0.0"
   val PekkoProjectionVersion = "1.0.0"
   val PekkoProjectionVersionInDocs = "1.0.0"
