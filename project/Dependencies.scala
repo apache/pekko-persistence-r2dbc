591b704f89f8fe8ed667346d9021ca41c9e3d56a
PJ Fanning <pjfanning@users.noreply.github.com>
2023-11-03 09:05:59 +0100

    use latest scala releases (#65)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,8 +15,8 @@ import sbt._
 
 object Dependencies {
   val Scala212 = "2.12.18"
-  val Scala213 = "2.13.11"
-  val Scala3 = "3.3.0"
+  val Scala213 = "2.13.12"
+  val Scala3 = "3.3.1"
   val PekkoVersion = System.getProperty("override.pekko.version", "1.0.1")
   val PekkoVersionInDocs = "1.0"
   val PekkoPersistenceJdbcVersion = "1.0.0"
