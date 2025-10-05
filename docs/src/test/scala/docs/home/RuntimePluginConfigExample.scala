60d5751949c6bd8e02579777d20353ddf9460237
Domantas Petrauskas <5850190+ptrdom@users.noreply.github.com>
2025-10-05 03:45:34 +0300

    Projection support for runtime persistence plugin config (#239)
    * Add projection initialization methods that allow passing runtime config
    
    * Add tests
    
    * Add documentation
    
    * Add missing license header
    
    * Address review comments
    
    * Remove site preview plugin dependency
    
    * Address review comments

--- docs/src/test/scala/docs/home/RuntimePluginConfigExample.scala
+++ docs/src/test/scala/docs/home/RuntimePluginConfigExample.scala
@@ -29,10 +29,12 @@ import com.typesafe.config.ConfigFactory
 object RuntimePluginConfigExample {
 
   type Command
+  type Event
+  type State
   val context: ActorContext[_] = ???
 
   // #runtime-plugin-config
-  def eventSourcedBehaviorForDatabase(database: String) = {
+  class EventSourcedBehaviorForDatabase(database: String) {
     val configKey = s"config-for-$database"
 
     val config: Config =
@@ -56,8 +58,8 @@ object RuntimePluginConfigExample {
             )
         )
 
-    (persistenceId: String) =>
-      EventSourcedBehavior[Command, String, String](
+    def apply(persistenceId: String) =
+      EventSourcedBehavior[Command, Event, State](
         PersistenceId.ofUniqueId(persistenceId),
         emptyState = ???,
         commandHandler = ???,
@@ -68,11 +70,11 @@ object RuntimePluginConfigExample {
         .withSnapshotPluginConfig(Some(config))
   }
 
-  val eventSourcedBehaviorForDatabase1 = eventSourcedBehaviorForDatabase("database-1")
+  val eventSourcedBehaviorForDatabase1 = new EventSourcedBehaviorForDatabase("database-1")
   context.spawn(eventSourcedBehaviorForDatabase1("persistence-id-1"), "Actor-1")
   context.spawn(eventSourcedBehaviorForDatabase1("persistence-id-2"), "Actor-2")
 
-  val eventSourcedBehaviorForDatabase2 = eventSourcedBehaviorForDatabase("database-2")
+  val eventSourcedBehaviorForDatabase2 = new EventSourcedBehaviorForDatabase("database-2")
   context.spawn(eventSourcedBehaviorForDatabase2("persistence-id-1"), "Actor-3")
   context.spawn(eventSourcedBehaviorForDatabase2("persistence-id-2"), "Actor-4")
   // #runtime-plugin-config
