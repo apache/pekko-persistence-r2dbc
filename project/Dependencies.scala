4fcb9192d5ef15b4a462b96d46b90ddc03180a13
Scala Steward <scala_steward@virtuslab.com>
2026-01-17 16:29:07 +0000

    Update postgresql to 42.7.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -59,7 +59,7 @@ object Dependencies {
       "org.apache.pekko" %% "pekko-projection-durable-state" % PekkoProjectionVersion % Test
     val pekkoProjectionTestKit = "org.apache.pekko" %% "pekko-projection-testkit" % PekkoProjectionVersion % Test
 
-    val postgresql = "org.postgresql" % "postgresql" % "42.7.8" % Test
+    val postgresql = "org.postgresql" % "postgresql" % "42.7.9" % Test
 
     val logback = "ch.qos.logback" % "logback-classic" % "1.5.24" % Test
     val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19" % Test
