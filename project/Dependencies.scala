359ed8d1648ac9c73bcbc6e5a94267b29fb7b10b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-01-28 20:10:57 +0100

    Update logback-classic to 1.5.26 (#298)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
 
     val postgresql = "org.postgresql" % "postgresql" % "42.7.9" % Test
 
-    val logback = "ch.qos.logback" % "logback-classic" % "1.5.25" % Test
+    val logback = "ch.qos.logback" % "logback-classic" % "1.5.26" % Test
     val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19" % Test
     val junit = "junit" % "junit" % "4.13.2" % Test
     val junitInterface = "com.github.sbt" % "junit-interface" % "0.13.3" % Test
