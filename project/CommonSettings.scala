48dc12fd687db8fb6b3f2e05811a07cd144432df
虎鸣 <hepin.p@alibaba-inc.com>
2026-06-18 19:22:17 +0800

    fix: make -Yfuture-lazy-vals conditional for Scala 3.9+
    Motivation:
    Scala 3.9 removed the -Yfuture-lazy-vals compiler option as the behavior
    is now the default. Passing this flag causes compilation errors on Scala 3.9+.
    
    Modification:
    Conditionally add -Yfuture-lazy-vals only when Scala minor version < 9
    using CrossVersion.partialVersion check.
    
    Result:
    Project compiles on both Scala 3.3.x (which needs the flag) and
    Scala 3.9+ (which has the behavior by default).

--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -34,15 +34,14 @@ object CommonSettings extends AutoPlugin {
         "-language:existentials")
       if (scalaBinaryVersion.value == "3")
         commonWconf ++ Seq(
-          "-Yfuture-lazy-vals",
           "-release:17",
           "-Wconf:msg=Implicit parameters should be provided with a `using` clause:s",
           "-Wconf:msg=is deprecated for wildcard arguments of types:s",
           "-Wconf:msg=The trailing ` _` for eta-expansion is unnecessary:s",
           "-Wconf:msg=with as a type operator has been deprecated:s",
           "-Wconf:msg=Unreachable case except for null:s",
-          "-Wconf:msg=is no longer supported for vararg splices:s",
-          "-Wconf:msg=bad option.*-Yfuture-lazy-vals:s")
+          "-Wconf:msg=is no longer supported for vararg splices:s") ++
+        (if (CrossVersion.partialVersion(scalaVersion.value).exists(_._2 < 9)) Seq("-Yfuture-lazy-vals", "-Wconf:msg=bad option.*-Yfuture-lazy-vals:s") else Seq.empty)
       else commonWconf
     },
     Test / logBuffered := false,
